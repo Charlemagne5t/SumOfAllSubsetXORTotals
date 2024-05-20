7825bdcccb56e23c0a4737678f2d22f03cd329c7
alek <wait82985@gmail.com>
2024-05-20 14:21:01 +0500

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,5 +1,18 @@
 public class Solution {
+    int res = 0;
     public int subsetXORSum(int[] nums) {
+        for(int i = 0; i < nums.length; i++) {
+            dfs(nums, i, nums[i]);
+        }
+
+        return res;
+    }
+
+    void dfs(int[] nums, int i, int xor) {
+        res += xor;
+        for(int j = i + 1; j < nums.length; j++) {
+            dfs(nums, j, xor ^ nums[j]);
+        }
 
     }
 }
